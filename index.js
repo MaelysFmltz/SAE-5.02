7529212d7e22529b4342a61c554fd5fb7de260da
Ignorantum <125766863+1gn0r4ntum@users.noreply.github.com>
2026-09-09 15:03:12 +0200

    premiers tests html

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
-
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './src/config/database.js'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API Node.js lancée et Base de données connectée');
-});
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur http://localhost:${PORT}`);
