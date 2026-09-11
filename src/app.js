bcc05a2b06c3ad89e3b6319761cd5e9d1183be4b
Ignorantum <125766863+1gn0r4ntum@users.noreply.github.com>
2026-09-11 14:21:29 +0200

    backend profil (ajouts fichiers nécessaires)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
+const profileRoutes = require('./routes/profileRoutes');
 
 const app = express();
 
